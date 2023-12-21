ec497eb5c9062c7ee7ca8dcb004e697229bbb19e
sanketrakhewar <sanket1rakhewar@gmail.com>
2023-12-21 23:19:38 +0530

    second commit

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function getWeather() {
     }
 
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
     fetch(currentWeatherUrl)
         .then(response => response.json())
@@ -19,26 +18,14 @@ function getWeather() {
             console.error('Error fetching current weather data:', error);
             alert('Error fetching current weather data. Please try again.');
         });
-
-    fetch(forecastUrl)
-        .then(response => response.json())
-        .then(data => {
-            displayHourlyForecast(data.list);
-        })
-        .catch(error => {
-            console.error('Error fetching hourly forecast data:', error);
-            alert('Error fetching hourly forecast data. Please try again.');
-        });
 }
 
 function displayWeather(data) {
     const tempDivInfo = document.getElementById('temp-div');
     const weatherInfoDiv = document.getElementById('weather-info');
     const weatherIcon = document.getElementById('weather-icon');
-    const hourlyForecastDiv = document.getElementById('hourly-forecast');
 
     weatherInfoDiv.innerHTML = '';
-    hourlyForecastDiv.innerHTML = '';
     tempDivInfo.innerHTML = '';
 
     if (data.cod === '404') {
@@ -73,6 +60,3 @@ function showImage() {
     weatherIcon.style.display = 'block'; 
 }
 
-function displayHourlyForecast(hourlyData) {
- const hourlyForecastDiv = document.getElementById('hourly-forecast');
-}
